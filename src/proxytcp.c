df6d9ab1e85b18ea0c99ddc5ea4bcf3d7270c145
holgidonath <hdonath@itba.edu.ar>
2021-11-06 17:13:13 -0300

    Empezando con Proxy TCP

---
+++ src/proxytcp.c
@@ -0,0 +1,252 @@
+/**
+ * main.c - servidor proxy socks concurrente
+ *
+ * Interpreta los argumentos de línea de comandos, y monta un socket
+ * pasivo.
+ *
+ * Todas las conexiones entrantes se manejarán en éste hilo.
+ *
+ * Se descargará en otro hilos las operaciones bloqueantes (resolución de
+ * DNS utilizando getaddrinfo), pero toda esa complejidad está oculta en
+ * el selector.
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <errno.h>
+#include <signal.h>
+
+#include <unistd.h>
+#include <sys/types.h>   // socket
+#include <sys/socket.h>  // socket
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+
+#include "selector.h"
+#include "buffer.h"
+
+
+
+
+typedef struct client
+{
+
+    int client_fd;
+
+
+} client;
+
+typedef struct origin 
+{
+    int origin_fd;
+    struct sockaddr_storage origin_addr;
+    socklen_t origin_addr_len;
+    struct addrinfo *origin_resolution;
+    struct addrinfo *origin_resolution_current;
+
+
+} origin;
+
+struct connection 
+{
+    client * client;
+    origin * origin;
+    buffer read_buffer, write_buffer;
+    uint8_t raw_buff_a[2048], raw_buff_b[2048];
+};
+
+
+struct connection * 
+new_connection(int client_fd)
+{
+    struct connection * con;
+    con = malloc(sizeof(*con));
+    if (con != NULL)
+    {
+        memset(con, 0x00, sizeof(*con));
+        con->origin->origin_fd = -1;
+        con->client->client_fd = client_fd;
+        buffer_init(con->read_buffer, N(CON-))
+
+
+    }
+}
+
+void
+proxy_tcp_connection(struct selector_key *key){
+    struct sockaddr_storage       client_addr;
+    socklen_t                     client_addr_len = sizeof(client_addr);
+   
+
+    const int client = accept(key->fd, (struct sockaddr*) &client_addr, &client_addr_len);
+    if(client == -1) {
+        goto fail;
+    }
+    if(selector_fd_set_nio(client) == -1) {
+        goto fail;
+    }
+    struct connection *connection = new_connection(client);
+    // if(state == NULL) {
+    //     // sin un estado, nos es imposible manejaro.
+    //     // tal vez deberiamos apagar accept() hasta que detectemos
+    //     // que se liberó alguna conexión.
+    //     goto fail;
+    // }
+
+    memcpy(&connection->client->client_addr, &client_addr, client_addr_len);
+    connection->client->client_addr_len = client_addr_len;
+
+    // Falta inicializar estructura de connection y setear stm
+
+    if(SELECTOR_SUCCESS != selector_register(key->s, client, NULL, NULL, NULL)) {
+        goto fail;
+    }
+    // Falta hacer el connect al ORIGIN
+    return ;
+fail:
+    if(client != -1) {
+        close(client);
+    }
+}
+
+
+static bool done = false;
+
+static void
+sigterm_handler(const int signal) {
+    printf("signal %d, cleaning up and exiting\n",signal);
+    done = true;
+}
+
+int
+main(const int argc, const char **argv) {
+    unsigned port = 1080;
+
+    if(argc == 1) {
+        // utilizamos el default
+    } else if(argc == 2) {
+        char *end     = 0;
+        const long sl = strtol(argv[1], &end, 10);
+
+        if (end == argv[1]|| '\0' != *end 
+           || ((LONG_MIN == sl || LONG_MAX == sl) && ERANGE == errno)
+           || sl < 0 || sl > USHRT_MAX) {
+            fprintf(stderr, "port should be an integer: %s\n", argv[1]);
+            return 1;
+        }
+        port = sl;
+    } else {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        return 1;
+    }
+
+    // no tenemos nada que leer de stdin
+    close(0);
+
+    const char       *err_msg = NULL;
+    selector_status   ss      = SELECTOR_SUCCESS;
+    fd_selector selector      = NULL;
+
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family      = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port        = htons(port);
+
+    const int server = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(server < 0) {
+        err_msg = "unable to create socket";
+        goto finally;
+    }
+
+    fprintf(stdout, "Listening on TCP port %d\n", port);
+
+    // man 7 ip. no importa reportar nada si falla.
+    setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
+
+    if(bind(server, (struct sockaddr*) &addr, sizeof(addr)) < 0) {
+        err_msg = "unable to bind socket";
+        goto finally;
+    }
+
+    if (listen(server, 20) < 0) {
+        err_msg = "unable to listen";
+        goto finally;
+    }
+
+    // registrar sigterm es útil para terminar el programa normalmente.
+    // esto ayuda mucho en herramientas como valgrind.
+    signal(SIGTERM, sigterm_handler);
+    signal(SIGINT,  sigterm_handler);
+
+    if(selector_fd_set_nio(server) == -1) {
+        err_msg = "getting server socket flags";
+        goto finally;
+    }
+    const struct selector_init conf = {
+        .signal = SIGALRM,
+        .select_timeout = {
+            .tv_sec  = 10,
+            .tv_nsec = 0,
+        },
+    };
+    if(0 != selector_init(&conf)) {
+        err_msg = "initializing selector";
+        goto finally;
+    }
+
+    selector = selector_new(1024);
+    if(selector == NULL) {
+        err_msg = "unable to create selector";
+        goto finally;
+    }
+    const struct fd_handler passive_accept_handler = {
+        .handle_read       = proxy_tcp_connection,
+        .handle_write      = NULL,
+        .handle_close      = NULL, // nada que liberar
+    };
+    ss = selector_register(selector, server, &passive_accept_handler,
+                                              OP_READ, NULL);
+    if(ss != SELECTOR_SUCCESS) {
+        err_msg = "registering fd";
+        goto finally;
+    }
+    for(;!done;) {
+        err_msg = NULL;
+        ss = selector_select(selector);
+        if(ss != SELECTOR_SUCCESS) {
+            err_msg = "serving";
+            goto finally;
+        }
+    }
+    if(err_msg == NULL) {
+        err_msg = "closing";
+    }
+
+    int ret = 0;
+finally:
+    if(ss != SELECTOR_SUCCESS) {
+        fprintf(stderr, "%s: %s\n", (err_msg == NULL) ? "": err_msg,
+                                  ss == SELECTOR_IO
+                                      ? strerror(errno)
+                                      : selector_error(ss));
+        ret = 2;
+    } else if(err_msg) {
+        perror(err_msg);
+        ret = 1;
+    }
+    if(selector != NULL) {
+        selector_destroy(selector);
+    }
+    selector_close();
+
+    socksv5_pool_destroy();
+
+    if(server >= 0) {
+        close(server);
+    }
+    return ret;
+}
+
+
