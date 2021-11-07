9b6b143c3720572f94a46c9c02b6d95577e8d2cc
Nicolas Rossi <nrossi@itba.edu.ar>
2021-11-07 11:57:41 -0300

    some clean up to work in main proxy

--- TP1/include/buffer.h
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-#define BUFFSIZE 1024
-struct buffer {
-	char * buffer;
-	size_t len;     // longitud del buffer
-	size_t from;    // desde donde falta escribir
-};
--- src/logger.c
+++ src/logger.c
@@ -1,4 +1,4 @@
-#include "./include/logger.h"
+#include "include/logger.h"
 
 LOG_LEVEL current_level = DEBUG;
 
