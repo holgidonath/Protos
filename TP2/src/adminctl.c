f5196dd96056ce18b4cfa32a2e00f7ab13ee425c
holgidonath <hdonath@itba.edu.ar>
2021-11-21 17:17:22 -0300

    starting admin
    Co-Authored-By: Rodrigo Fera <rfera@itba.edu.ar>

--- TP2/src/adminctl.c
+++ TP2/src/adminctl.c
@@ -15,25 +15,56 @@
 #include <stdbool.h>
 
 #include "include/argsctl.h"
-#include "include/logger.h"
+
+int get_authentication(int sock,char * in_buffer,char * out_buffer)
+{
+    int n;
+    int nr;
+    fgets(out_buffer, BUFF_SIZE, stdin);
+    strtok(out_buffer, "\n");
+    if (n = sctp_sendmsg(sock, out_buffer, strlen(out_buffer),
+                         NULL, 0, 0, 0, 0, 0, 0) < 0) {
+        printf("ERROR");
+        return -1;
+    }
+    nr = sctp_recvmsg(sock, in_buffer, BUFF_SIZE, NULL, 0, 0, 0);
+    if (nr <= 0) {
+        printf("ERROR");
+        return -1;
+    }
+    if (in_buffer[0] == '+')
+    {
+        return  1;
+    }
+    else
+    {
+        return 0;
+    }
+
+}
+
 
 int main(const int argc, char **argv) {
+
+    int status = 0;
     struct admin_opt opt;
     struct address_data addr;
-    char out[BUFF_SIZE];
-    char incoming[BUFF_SIZE];
+    printf("hola");
+    char out[BUFF_SIZE] = {0};
+    char incoming[BUFF_SIZE] = {0};
     parse_admin_options(argc, argv, &opt);
-    set_mgmt_address(&addr, opt.mgmt_addr);
+    set_mgmt_address(&addr, opt.mgmt_addr, &opt);
     int sock = socket(addr.mgmt_domain, SOCK_STREAM, IPPROTO_SCTP);
+    printf("hola");
     if(sock < 0)
     {
-        log(ERROR, "Failed to create socket");
+        printf("Failed to create socket");
         close(sock);
     }
-    int con = connect(sock, (struct sockaddr*)addr.mgmt_addr, addr.mgmt_addr_len);
+    int con = connect(sock, (struct sockaddr*)&addr.mgmt_addr, addr.mgmt_addr_len);
     if (con < 0)
     {
-        log(ERROR, "Failed to connect to management");
+        printf("Failed to connect to management");
         close(sock);
     }
     int n = sctp_recvmsg(sock, incoming, BUFF_SIZE, NULL, 0,0,0);
@@ -44,9 +75,25 @@ int main(const int argc, char **argv) {
         exit(EXIT_FAILURE);
     }
 
+    while(status == 0);
+    {
+        printf("Please enter Password to enter. You may type 'QUIT to exit\n Password: ");
+        status = get_authentication(sock,incoming, out);
 
-
-
+    }
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
